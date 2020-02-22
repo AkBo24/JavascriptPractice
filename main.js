f3f54c2e29bcbbab0c3f61991644a0dca2c7fb3a
AkBo24 <akshaybodla21@gmail.com>
2020-02-22 16:08:04 -0600

    Basics of data types

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert('Hello World');
+//var, let, const - three types of variables in java
+
+//var - dont use
+
+//let - allows reasinging 
+let z = 30;
+z = 31;
+
+//const - constant variable in java
+//always use unless variable is gauranteed to be changed
+//ex:
+const MAX_GAME_SCORE = 90;
+
+//data types: directly assinged to memory not a resource
+//String, Numbers, Boolean, null, jundefined, Symbol
+
+const name  = 'James';
+const age   = 9;
+const isMan = true;
+const x     = null;
+const y     = undefined;
+// y = 8; does not work bc y is constant
