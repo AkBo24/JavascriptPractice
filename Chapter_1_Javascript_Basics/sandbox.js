7b5054e27cf398dfee107a7c330eea13a3bc7c65
AkBo24 <akshaybodla21@gmail.com>
2020-03-18 15:48:15 -0500

    basic string methods

---
+++ Chapter_1_Javascript_Basics/sandbox.js
@@ -0,0 +1,26 @@
+// alert("This got updated!");
+
+//Log to website console
+// console.log("Logging to console!");
+
+//Variable Types:
+let age = 19; //normal java variable
+const yearBorn = 2019; //java's final modifier
+var year = 2020; //older way of variable, let preferred
+
+//Common Javascript String methods
+let email = "akshaybodla21@gmail.com";
+
+//.lastIndexOf() - gets the last instance of that character
+console.log(email.lastIndexOf("a"));
+
+//.slice() - java's .substring()
+//last pos inclusive
+console.log(email.slice(0,3));
+
+//.substring() - 2 args: pos to start at, #of characters to get
+//NOT LIKE JAVA SUBSTRING!!!
+console.log(email.substr(5,3));
+
+//.replace() - replace the first instance of this chara with other
+console.log(email.replace('@','#'));
