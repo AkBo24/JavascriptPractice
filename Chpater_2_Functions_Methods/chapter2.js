262da2ff78952afa25033c4bd779b68324895a9e
AkBo24 <akshaybodla21@gmail.com>
2020-03-19 12:55:37 -0500

    Default arguments & return types

--- Chpater_2_Functions_Methods/chapter2.js
+++ Chpater_2_Functions_Methods/chapter2.js
@@ -5,7 +5,7 @@
 function greet() {
     console.log("Good Morning!");
 }
-// greet();
+greet();
 
 //Function Expressions:
 let morning = function () {
@@ -15,3 +15,17 @@ morning();
 
 //Difference: Function Expressions can not be declared below their use 
 //where declarations can be used b4 declaration
+
+//Add default arguments to parameter list in function expressions:
+let greetings = function(name = "luigi", time = "morning") {
+    console.log(`Good ${time} ${name}`);
+};
+greetings();//Will log Good morning luigi
+
+//Functions do not need a declared return type, just return a value
+let calcArea = function(radius) {
+    let area = 3.14 * radius**2;
+    return area;
+};
+console.log(calcArea(10));
+
