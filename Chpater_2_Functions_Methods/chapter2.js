34e793a8b8eb6be93cdb35b93bfdf8af812d4908
AkBo24 <akshaybodla21@gmail.com>
2020-03-19 13:02:01 -0500

    arrow functions

--- Chpater_2_Functions_Methods/chapter2.js
+++ Chpater_2_Functions_Methods/chapter2.js
@@ -23,9 +23,17 @@ let greetings = function(name = "luigi", time = "morning") {
 greetings();//Will log Good morning luigi
 
 //Functions do not need a declared return type, just return a value
-let calcArea = function(radius) {
+let circArea = function(radius) {
     let area = 3.14 * radius**2;
     return area;
 };
-console.log(calcArea(10));
+console.log(circArea(10));
 
+// Arrow Function - shorthand way of writing a function expression
+// if 1 parameter is needed, parethesis around the parameters can be removed
+// "this" key word works differently w/the arrow function
+let boxArea = (length, width) => { return length*width; };
+console.log(boxArea(10,10));
+
+// boxArea can be shorted to the following syntax
+// let boxArea = (length, width) => length*width; //no return keyword needed, value automatically returned
