1af00ddc44f41bc294fccba578fba7663730bd04
AkBo24 <akshaybodla21@gmail.com>
2020-03-19 12:44:21 -0500

    Function Declaration & Function Expressions

--- Chpater_2_Functions_Methods/chapter2.js
+++ Chpater_2_Functions_Methods/chapter2.js
@@ -1,1 +1,17 @@
 // alert("js is working");
+
+//Function Declararation:
+
+function greet() {
+    console.log("Good Morning!");
+}
+// greet();
+
+//Function Expressions:
+let morning = function () {
+    console.log("Hello There!");
+};
+morning();
+
+//Difference: Function Expressions can not be declared below their use 
+//where declarations can be used b4 declaration
