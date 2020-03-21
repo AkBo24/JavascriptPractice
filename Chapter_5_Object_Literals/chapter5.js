295699b37e16413116d9db7c90d8d75d610f5464
AkBo24 <akshaybodla21@gmail.com>
2020-03-21 18:12:22 -0500

    This Keyword in JS

--- Chapter_5_Object_Literals/chapter5.js
+++ Chapter_5_Object_Literals/chapter5.js
@@ -1,12 +1,30 @@
 //Object literals in JS
 let user = {
-    name: 'John Doe',
-    email: "johndoe@domain.com",
-    games: ["Hallow Knight", "Dragon Quest V"],
+    name  : 'John Doe',
+    email : "johndoe@domain.com",
+    games : ["Hallow Knight", "Dragon Quest V"],
+    logged: false,
     
     //creating method:
-    login: function() {
+    login () {
         console.log('user logged in');
+        isLogged = true;
+    },
+
+    login () {
+        console.log('user logged out');
+        isLogged = false;
+    },
+    //In js, can't directly access fields; use this keyword
+    isLogged () {
+        console.log(this.logged);
+    },
+
+    getGames () {
+        console.log(`${this.name} owns these games:`);
+        this.games.forEach(games => {
+            console.log(games);
+        })
     }
 };
 
@@ -14,4 +32,6 @@ console.log(typeof user);
 //access attributes
 console.log(user.games[0]); //dot notation
 console.log(user['name']);
+//Calling methods
 user.login();
+user.getGames();
