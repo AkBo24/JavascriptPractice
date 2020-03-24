6ca37b6c443af5faaac93d5cb7105ce52814b31e
AkBo24 <akshaybodla21@gmail.com>
2020-03-23 19:42:16 -0500

    updating & changing HTML through DOM and javascript

--- Chapter_6_Document_Object_Model/chapter6.js
+++ Chapter_6_Document_Object_Model/chapter6.js
@@ -3,6 +3,13 @@
 const para = document.querySelector('p'); //grabs the first p tag in the html
 console.log(para);
 
+//to get the inner text, access the innertext property:
+console.log(para.innerText);
+
+//to change the inner text, update the inner text variable:
+para.innerText +=`\n This paragraph was also changed in Javascript by updating the DOM's innerText
+                   property for this paragraph!`
+
 //Query for a paragraph using a class selector (grabs the first element with the indicated class)
 const para2 = document.querySelector('.query'); //the "." indicates to JS to look for a class
 console.log(para2);
@@ -14,6 +21,11 @@ console.log(div);
 //Select all of some type
 const paras = document.querySelectorAll('p');
 console.log(paras);
+
+//to change/update every element in the DOM (that the instance of the particular element)
+paras.forEach(element => {
+    paras.innerText += "This paragraph was updated using Javascript!";
+});
 
 //inside DOM paras is an array, methods are unavaible, but can still access elements inside Node List - like an array
 //w/ box notation
@@ -39,3 +51,6 @@ console.log(queryByClass); //DOM creates a HTMLCollection instead of a Node List
 //get element by tag name'
 const parasAll = document.getElementsByTagName('p');
 console.log(parasAll);
+
+const makeH2 = document.querySelector("#update");
+makeH2.innerHTML = '<h2>Quering the DOM: (this html was updated in JS!)</h2>'
