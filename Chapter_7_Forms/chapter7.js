5158e3bc26cb4f1887ae4847631f1fb0eb9bb21a
AkBo24 <akshaybodla21@gmail.com>
2020-03-29 12:17:59 -0500

    Key validation using RegEx

--- Chapter_7_Forms/chapter7.js
+++ Chapter_7_Forms/chapter7.js
@@ -1,15 +1,37 @@
-const form     = document.querySelector(".signup");
-const username = form.username;
+const form  = document.querySelector(".signup");
+const div   = document.querySelector(".feedback");
+const input = document.querySelector("#username");
+const regEx = /^[a-z]{6,}$/;
+let username;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     //username.value gives access to what the user types into the box
+    //otherwise use e.target.username.value to get what's typed
+    username = form.username;
 
     //RegEx are contained in two "/" (Regular Expression)
     //regEx offer a .test method that checks whether the passed in argument
     //satisfies the regEx
-    const regEx = /^[a-z]{6,}$/;
+    //  allow only characters a-z and at least 6 char long
     let  result = regEx.test(username.value);
     if(result)
-        console.log(result);
+        div.textContent = `${username.value} is a valid username`;
+    else 
+        div.textContent = `${username.value} is not a valid username`;
+
+});
+
+//Dynamic feedback: let the user know if the user name is valid while they type
+form.username.addEventListener('keyup', e => {
+    const username = e.target;
+    let  result = regEx.test(username.value);
+    
+    if(result) {
+        form.username.setAttribute('class', 'valid');
+    }
+    else {
+        form.username.setAttribute('class', 'invalid');
+    }
+    
 });
