9073f8d82019f2b2a65c42add94fa8001b25418d
AkBo24 <akshaybodla21@gmail.com>
2020-03-29 11:13:04 -0500

    Submit event form

---
+++ Chapter_7_Forms/chapter7.js
@@ -0,0 +1,9 @@
+const form     = document.querySelector(".signup");
+const username = form.username;
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    //username.value gives access to what the user types into the box
+
+    //testing RegEx
+});
