a847cef19a25f13864f08fb5df7fee4896ef4684
AkBo24 <akshaybodla21@gmail.com>
2020-03-29 11:23:35 -0500

    Reg Ex basics

--- Chapter_7_Forms/chapter7.js
+++ Chapter_7_Forms/chapter7.js
@@ -5,5 +5,11 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     //username.value gives access to what the user types into the box
 
-    //testing RegEx
+    //RegEx are contained in two "/" (Regular Expression)
+    //regEx offer a .test method that checks whether the passed in argument
+    //satisfies the regEx
+    const regEx = /^[a-z]{6,}$/;
+    let  result = regEx.test(username.value);
+    if(result)
+        console.log(result);
 });
