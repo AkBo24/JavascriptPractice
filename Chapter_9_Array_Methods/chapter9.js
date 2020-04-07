389de280ed22bd4771de8effcac7e5fdde390c71
AkBo24 <akshaybodla21@gmail.com>
2020-04-07 10:37:35 -0700

    Filter Method

--- Chapter_9_Array_Methods/chapter9.js
+++ Chapter_9_Array_Methods/chapter9.js
@@ -1,1 +1,18 @@
-alert("js works")
+/**
+ * Array Methods: filter
+ * 
+ * Filter is a callback method that can give access to individual elements
+ */
+
+ const people = [
+     {name: "Ak", age: 19},
+     {name: "Padmaja", age: 50},
+     {name: "Prasad", age: 55},
+     {name: "Padmaja", age: 26}
+ ]
+
+ const older = people.filter(person => {
+    return person.age > 30;
+ });
+
+ console.log(older);
