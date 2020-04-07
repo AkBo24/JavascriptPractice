b74a85f5201ea38abaf67b37a74939715b47931a
AkBo24 <akshaybodla21@gmail.com>
2020-04-07 10:50:23 -0700

    added array method map

--- Chapter_9_Array_Methods/chapter9.js
+++ Chapter_9_Array_Methods/chapter9.js
@@ -1,5 +1,5 @@
 /**
- * Array Methods: filter
+ * Array Method: filter
  * 
  * Filter is a callback method that can give access to individual elements
  */
@@ -16,3 +16,30 @@
  });
 
  console.log(older);
+
+ /**
+  * Array Method: Map
+  * 
+  * Map is also a callback method that iterates through the entire array
+  * and returns a  new array with the results for everything in the 
+  * origininal array
+  */
+
+const items = [
+    {name: "apples", price: 30, isSale: true},
+    {name: "oranges", price: 300, isSale: false},
+    {name: "tv", price: 1, isSale: true},
+    {name: "computer", price: 4440, isSale: true},
+    {name: "pencil", price: 0.125, isSale: true}
+]
+
+const sale = items.map(item =>{
+
+    if(item.isSale)
+        return {name: item.name, price: item.price/2};
+    else
+        return item
+
+});
+
+console.log(sale);
