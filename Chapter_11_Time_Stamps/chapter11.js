5bd24ae4b14077ace7471e5360b973b87421e2b6
AkBo24 <akshaybodla21@gmail.com>
2020-04-08 16:07:29 -0700

    Time object

---
+++ Chapter_11_Time_Stamps/chapter11.js
@@ -0,0 +1,17 @@
+/**
+ * Javascript has the time object 
+ * which has all of the following methods
+ */
+
+ const time = new Date();
+
+ console.log(time);
+ console.log(time.toDateString());
+ console.log(time.toTimeString());
+ console.log(time.toLocaleDateString())
+ console.log(time.getFullYear());
+
+ console.log(time.getHours());
+ console.log(time.getMinutes());
+ console.log(time.getSeconds());
+ console.log(time.getTime());
