ade61e5bf9a3acd367c4e8be8f038dc467b229a2
AkBo24 <akshaybodla21@gmail.com>
2020-04-09 15:35:22 -0700

    How to request data from an api

--- Chapter_12_Asyn_JS/chapter12.js
+++ Chapter_12_Asyn_JS/chapter12.js
@@ -27,4 +27,35 @@
   * instructions were run
   */
 
-  
+  /**
+   * One type of data we can request is an HTTP request from an API
+   * (usually as an endpoint URL    )
+   * 
+   * API used below: jsonplaceholder.typicode.com
+   */
+
+    /**
+     * Requesting/Getting data from an API
+     * 
+     * In JS the XMLHttpRequest object can handle many types of dat
+     *  (XML, JSON, Plaintext etc)
+     * 
+     * The object has 4 "states" that indicate what its doing
+     *  0: client created but open() has not been called
+     *  1: open() has been called
+     *  2: sent() has been called adn status is available
+     *  3: responseText field has partial data sent by the API
+     *  4: operation is complete
+     * 
+     * To request data from an api the open() method allows the object 
+     *  to inform what operation with 2 parameter
+     *      1: the operation type (get, update etc)
+     *      2: the api endpoint url
+     * 
+     * The sent() method sends the operation defined in open() to the api
+     * 
+     * To do event handeling, like printing the api data add an 
+     *  event listener to the request object
+     */
+
+     
