74885cc144aac484764916c1dcf066a42b642a0f
AkBo24 <akshaybodla21@gmail.com>
2020-04-21 21:31:03 -0700

    Updates all data not images

--- Weather_App/scripts/app.js
+++ Weather_App/scripts/app.js
@@ -1,14 +1,6 @@
-const input = document.querySelector('form');
-
-const updateData = async (city) => {
-    const getCity = await getCityData(city);
-    const getWeather = await getCityWeather(getCity.Key);
-
-    return {
-        getCity,
-        getWeather
-    };
-}
+const input    = document.querySelector('form');
+const details  = document.querySelector(".details");
+const card     = document.querySelector(".card");
 
 input.addEventListener('submit', e => {
     //prevent reload
@@ -25,3 +17,28 @@ input.addEventListener('submit', e => {
         .catch(err => console.log(err));
 });
 
+const updateData = async (city) => {
+    const getCity = await getCityData(city);
+    const getWeather = await getCityWeather(getCity.Key);
+
+    return {
+        getCity,
+        getWeather
+    };
+
+}
+
+const updateUI = (data) => {
+    const {getCity, getWeather} = city;
+
+    details.innerHTML = `
+        <h5 class="my-3">${getCity.EnglishName}</h5>
+        <div class="my-3">${getWeather.WeatherText}</div>
+        <div class = "display-4 my-4">
+            <span>${getWeather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>    
+    `
+    if(card.classList.contains("d-none"))
+        card.classList.remove("d-none");
+}
