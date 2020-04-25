4e3702cd388f74498e869bd04cbefd0fc5b08a76
AkBo24 <akshaybodla21@gmail.com>
2020-04-24 17:23:28 -0700

    Prototype model

---
+++ Chapter_15_OOP/prototype.js
@@ -0,0 +1,18 @@
+/**
+ * Javascript implements the Object class using a __prototype__ found
+ * on each class
+ * 
+ * The __prototype__ provides the base functions for each class
+ * 
+ * Type Classname.prototype to see an object's prototytpe
+ */
+
+/**
+ * B4 the class keyword to create classes one would have to define a
+ * constructor function (just as you would in java) and then add
+ * methods to the prototype for that 
+ * 
+ * To manually add to a user defined prototype, 
+ *  Classname.prototype.methodname = function() {
+ *  }
+ */
