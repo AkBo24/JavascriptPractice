d28c0b7bef54a84f4bb25f73333f6632b61dbf5e
AkBo24 <akshaybodla21@gmail.com>
2020-04-25 11:16:32 -0700

    Finding the id and deleting a document

--- Chapter_16_Firebase/firebaseIntro.js
+++ Chapter_16_Firebase/firebaseIntro.js
@@ -25,26 +25,27 @@ const input = document.querySelector("form")
 
 database.collection("alchemy").get()
     .then(snapshot => {
-        snapshot.docs.forEach(doc => addrecipe(doc.data()))
+        snapshot.docs.forEach(doc => addrecipe(doc.data(), doc.id))
         //addrecipe(doc.data())
     })
     .catch( err => console.log(err));
 
-const addrecipe = (doc) => {
+const addrecipe = (doc, id) => {
     // console.log(doc.Result) -> prints the result of each subcollection
     const result = doc.Result;
     const time   = doc.createdAt.toDate();
-    console.log(doc)
+    // console.log(doc)
     list.innerHTML += `
-        <li>
+        <li data-id="${id}">
             <div>Result: ${result}</div>
             <div>Created at: ${time}</div>
+            <button class = "btn btn-danger btn-sm my-2">delete</button>
         </li>
     `;
 };
 
 /**
- * To add to the database theh collections api has a .add() function
+ * To add to the database the collections api has a .add() function
  * which takes a JS object formatted to the collection and creates a new
  * document as needed
  */
@@ -61,3 +62,19 @@ input.addEventListener("submit", e => {
         .then(() => console.log("added successfully"))
         .catch(err => console.log(err));
 });
+
+/**
+ * To delete from the database:
+ *      .delete(*id*)
+ */
+
+list.addEventListener('click', e => {
+    //console.log(e.target.tagName)
+    if(e.target.tagName == "BUTTON") {
+        const id = e.target.parentElement.getAttribute("data-id");
+        // console.log(id);
+        database.collection("alchemy").doc(id).delete()
+            .then(() => console.log("Successfully deleted"))
+            .catch(err => console.log(err));
+    }
+})
