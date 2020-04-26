b036a406dbe604a3605c5cab5b8df46da6a6b5c0
AkBo24 <akshaybodla21@gmail.com>
2020-04-26 13:11:28 -0700

    time and add css

--- Chatroom_App/scripts/ui.js
+++ Chatroom_App/scripts/ui.js
@@ -4,14 +4,14 @@ class ChatUI {
     }
 
     render(data) {
-        
-
+        const timeDiff = dateFns.distanceInWordsToNow
+            (data.createdat.toDate(), {suffix: true})
 
         const html = `
             <li class="list-group-item">
                 <span class = "username">${data.username}</span>
                 <span class = "message">${data.message}</span>
-                <div class = "time">${data.createdat.toDate()}</span>
+                <div class = "time">${timeDiff}</span>
             </li>
         `;
 
