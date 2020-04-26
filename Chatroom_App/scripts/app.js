8e16424b2790cbe5e930497112520a7523bf8190
AkBo24 <akshaybodla21@gmail.com>
2020-04-26 14:31:01 -0700

    Username & localstorage

--- Chatroom_App/scripts/app.js
+++ Chatroom_App/scripts/app.js
@@ -1,6 +1,34 @@
-const chats = document.querySelector(".chat-list");
+const chats    = document.querySelector(".chat-list");
+const userMssg = document.querySelector(".new-chat");
+const updateName = document.querySelector(".new-name");
+
+console.log(updateName.name)
 
 const chatUI = new ChatUI(chats);
-const chatroom = new Chatroom('#gamers','sultan');
+let chatroom = null;
+
+if(localStorage.getItem('username') == null)
+    chatroom = new Chatroom('#gamers','anon');
+else   
+    chatroom = new Chatroom('#gamers', localStorage.getItem('username'));
+
 
 chatroom.getChats(data => chatUI.render(data));
+
+userMssg.addEventListener("submit", e => {
+    e.preventDefault();
+    const newMessage = userMssg.message.value.trim();
+    chatroom.addChat(newMessage)
+        .then(() => userMssg.reset())
+        .catch(err => console.log(err))
+})
+
+updateName.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const newName = updateName.name.value.trim();
+    chatroom.setUserName(newName);
+    console.log(`new name: ${newName}`)
+    localStorage.setItem('username', newName);
+
+});
