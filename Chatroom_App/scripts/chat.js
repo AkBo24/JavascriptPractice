2e0518b36cd75e7a315b4b9ad6290eb4cffdded3
AkBo24 <akshaybodla21@gmail.com>
2020-04-26 10:49:57 -0700

    Get data when firestore db changes

--- Chatroom_App/scripts/chat.js
+++ Chatroom_App/scripts/chat.js
@@ -25,7 +25,25 @@ class Chatroom {
         //object
         const addMssg = await this.chats.add(mssg);
     }
+
+    /**
+     * Update the chatroom when there is an update to the chatroom 
+     * database
+     * @param {Callback Function} updateUi 
+     */
+    async getChats (updateUi) {
+        this.chats.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type === 'added')
+                    return updateUi(change.doc.data());
+            });
+
+        });
+    }
 }
 
 const chatroom = new Chatroom("#gamers", "#me");
-chatroom.addChat("First chat message");
+// chatroom.addChat("First chat message");
+chatroom.getChats((data) => 
+    {console.log(data)
+});
