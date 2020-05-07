903525ac9bd9579e32e1863f5bcdfda9b69b68cd
AkBo24 <akshaybodla21@gmail.com>
2020-05-07 09:40:34 -0700

    Spread syntax - (array, object)

--- Chpater_18_ES6/spread&rest.js
+++ Chpater_18_ES6/spread&rest.js
@@ -15,3 +15,19 @@ const printNums = (...params) => {
 };
 
 const params = printNums(9, 9.9, 3.14, 5.1, 2.2);
+
+/**
+ * Spread Syntax:
+ * 
+ * For - "Breaking down" an array or object into its individual elements
+ * 
+ * Syntax: ...*array/object name*
+ */
+
+const arr1 = ['me', 'you'];
+const arr2 = [...arr1, 'i'];
+console.log(arr2);
+
+const obj1 = {name: "Ak", age: 19};
+const obj2 = {last: "bo", ...obj1, hobby:"gaming"};
+console.log(obj2);
