0297ff226e84c07c1a652675243b4b7fe15461b0
AkBo24 <akshaybodla21@gmail.com>
2020-05-08 18:08:00 -0700

    Async read/write file

--- NodeJS/ReadWrite.js
+++ NodeJS/ReadWrite.js
@@ -5,18 +5,24 @@
 *
 *  Sync:
 *     1) readFileSync(*fileName*, *character set*);
-*     2) writeFileSync(*fileName*, *character set*);
+*     2) writeFileSync(*fileName*, *string data to write*);
 *
 *  Async
 *     1) readFile(*fileName*, *character set*, (err, data) => {});
-*     2) writeFileSync(*fileName*, data); where data is the string to put into the file
+*     2) writeFileSync(*fileName*, data , (err) => {});
+*        where data is the string to put into the file
 *
 *  For write file, if it doesn't exist, node will create it
 */
 
 const fs = require("fs");
 
-/*Writing and reading files synchronously*/
+/* Writing and reading files synchronously */
 // const read = (fs.readFileSync("readMe.txt", "utf8"));
 // console.log(read);
 // fs.writeFileSync("result.txt", read);
+
+fs.readFile('readMe.txt', 'utf8', (err, data) => {
+  console.log(data);
+  fs.writeFile('result.txt', data, (err) => console.log(err) );
+});
