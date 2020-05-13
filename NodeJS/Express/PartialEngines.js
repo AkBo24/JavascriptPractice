c34398a86f22ff26b78af87eab62c3c889c653b9
AkBo24 <akshaybodla21@gmail.com>
2020-05-13 11:02:25 -0700

    Partial Template Engines

---
+++ NodeJS/Express/PartialEngines.js
@@ -0,0 +1,21 @@
+/*
+*  Partial Engines allow reusability of "partial" html code
+*
+*  Inside the engines to access a template: <%- include(*directory*) %>
+*
+*/
+
+const http    = require('http');
+const express = require('express');
+const app     = express();
+
+app.get('/', (req, res) => {
+  res.render('website.ejs');
+});
+
+app.get('/contacts/:id', (req, res) => {
+  res.render('contacts.ejs', {profile: req.params.id});
+});
+
+app.listen(3000, '127.0.0.1');
+console.log('Listening to port 3000');
